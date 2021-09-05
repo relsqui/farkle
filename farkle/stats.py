cf4dcbbb27083ca23a42e5b62940e97792f64e26
Finn Ellis <relsqui@chiliahedron.com>
2021-09-04 21:02:39 -0700

    round cache keys to 50

--- farkle/stats.py
+++ farkle/stats.py
@@ -4,26 +4,41 @@ import sys
 from .scoring import score_dice, dice_to_counts
 from .conditional_print import con_print
 
+
+def round50(i):
+  return round(i/50) * 50
+
 class Stats(object):
   farkle_chance = {0:1, 1:.67, 2:.44, 3:.28, 4:.16, 5:.8, 6:.2}
   ev_cache = [{}, {}, {}, {}, {}, {}, {}]
   cache_hits = 0
+  cache_misses = 0
   recursion_counter = 0
   temp_recursion_limit = 0
 
   @classmethod
+  def initialize(cls):
+    for points in range(0, 10050, 50):
+      cls.ev_cache[0][points] = 0
+      cls.ev_cache[1][points] = 25 + round(-points * 2/3)
+
+  @classmethod
   def ev_dice(cls, dice_count, score):
+    # real scores are always multiples of 50
+    # keep the precise one for doing math with, but
+    # use a rounded version for cache keys
+    rscore = round50(score)
     cls.recursion_counter += 1
     if cls.temp_recursion_limit > 0 and cls.recursion_counter > cls.temp_recursion_limit:
       raise RecursionError
-    if dice_count == 0:
-      con_print("no more dice")
+    if score > 10000:
+      # don't roll more dice when you can bank and win
+      con_print("max score reached")
       return 0
-    if dice_count == 1:
-      con_print("last die")
-      # 25 is (100 * 1/6) + (50 * 1/6), the only ways to score with one die
-      return 25 + round(-score * 2/3)
-    if not cls.ev_cache[dice_count].get(score):
+    if dice_count == 0:
+      con_print("hot dice!")
+    if cls.ev_cache[dice_count].get(rscore) is None:
+      cls.cache_misses += 1
       turn_score_sum = 0
       combo_count = 0
       con_print(f"calculating ev for rolling {dice_count} dice with {score} points")
@@ -33,18 +48,18 @@ class Stats(object):
         if turn_score == 0:
           con_print("farkle")
           turn_score = -score
-        elif extra_dice > 0:
+        else:
           con_print(f"checking ev of leftover {extra_dice} dice")
           ev_continue = cls.ev_dice(extra_dice, score + turn_score)
           if ev_continue > turn_score:
             turn_score += ev_continue
         turn_score_sum += turn_score
         combo_count += 1
-      cls.ev_cache[dice_count][score] = round(turn_score_sum / combo_count)
+      cls.ev_cache[dice_count][rscore] = round(turn_score_sum / combo_count)
     else:
       cls.cache_hits += 1
       con_print(f"using cached value for {dice_count} dice and {score} points")
-    return cls.ev_cache[dice_count][score]
+    return cls.ev_cache[dice_count][rscore]
 
   @classmethod
   def should_i_bank(cls, dice_count, score):
--- main.py
+++ main.py
@@ -6,28 +6,43 @@ from farkle.scoring import score_dice
 
 
 def print_ev_tables(full_table=False, f=sys.stdout):
-  set_print_condition(False)
-  tare_depth(2)
+  print_progress = f is not sys.stdout or not full_table
   if_print(full_table, "EV of rolling D dice with P points", file=f)
   if_print(full_table, "P \\ D\t1\t2\t3\t4\t5\t6", file=f)
   threshold = [0] * 7
   for points in range(0, 10050, 50):
-    if_print(f is not sys.stdout or not full_table, "Testing", points, "...", file=sys.stderr)
+    if_print(print_progress, "Testing", points, "... ", end="", file=sys.stderr)
     if_print(full_table, points, end="", file=f)
     for dice in range(1, 7):
+      if_print(print_progress, dice, end=" ", file=sys.stderr)
+      Stats.recursion_counter = 0
       ev = Stats.ev_dice(dice, points)
       if_print(full_table, f"\t{str(ev)}", end="", file=f)
       if ev > 0:
         threshold[dice] = points
-    if_print(full_table, file=f)
+    if_print(full_table, file=f, flush=True)
+    if_print(print_progress, file=sys.stderr)
   if_print(full_table, file=f)
   print("Highest score at which it's still worth rolling D dice:", file=f)
   for dice in range(1, 7):
     print(f"{dice}: {threshold[dice]}", file=f)
-  print(file=f)
+  print_cache_stats(f=f)
+
+def print_cache_stats(f=sys.stdout):
   cache_size = sum(len(Stats.ev_cache[i]) for i in range(7))
-  print(f"final cache size was {cache_size} items, we hit it {Stats.cache_hits} times", file=f)
+  print(f"Cache size: {cache_size} items. {Stats.cache_hits} hits, {Stats.cache_misses} misses.", file=f)
+
+def main():
+  set_print_condition(False)
+  tare_depth(2)
+  # Stats.temp_recursion_limit = 1000000
+  Stats.initialize()
+  try:
+    with open("evtable.txt", "w") as f:
+     print_ev_tables(full_table=True, f=f)
+  except RecursionError:
+    print("hit recursion limit.")
+    print_cache_stats()
 
 if __name__ == "__main__":
-  with open("evtable.txt", "w") as f:
-    print_ev_tables(full_table=True, f=f)
+  main()
